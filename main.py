9b1f301cb7f9223597c7e76e6c4bc7ea3ab32c97
angel-carvajal-coder <angelcaru10+github@gmail.com>
2021-03-21 14:40:44 +0100

    Files are now listed in the GUI as well

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ class App(Tk):
         # sidebar.size = "50x500"
         sidebar.grid(row=0, column=0)
 
+        mainPart = Frame(master)
+        mainPart.grid(row=0, column=1)
+
         self.sidebar = sidebar
+        self.mainPart = mainPart
 
         os.chdir(os.environ["USER" if os.name != "nt" else "userprofile"])
 
         self.directoryIndex = 0
-        self.fileIndex = 0
+        self.currentFileIndex = 0
 
         for folderOrFile in os.listdir():
             if os.path.isdir(folderOrFile):
@@ -22,14 +26,17 @@ class App(Tk):
                 self.directoryIndex += 1
             else:
                 self.renderFile(folderOrFile)
-                self.fileIndex += 1
+                self.currentFileIndex += 1
 
     def renderDirectory(self, folder):
+        print(f"[DIR] {folder}")
         label = Label(self.sidebar, text=folder)
         label.grid(row=self.directoryIndex, column=0)
 
     def renderFile(self, file):
-        pass
+        print(f"[FILE] {file}")
+        label = Label(self.mainPart, text=file)
+        label.grid(row=self.currentFileIndex, column=1)
 
 root = Tk()
 app = App(root)
