fbc7069feeb46680a6e687138d49cf651ae060f4
angel-carvajal-coder <angelcaru10+github@gmail.com>
2021-03-21 14:21:31 +0100

    Listing files and directories in terminal: they will be listed in the GUI soon

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import os
 from tkinter import *
 
 class App(Tk):
     def __init__(self, master):
         self.master = master
-        # self.title = "File explorer"
+
+        sidebar = Frame(master)
+        # sidebar.size = "50x500"
+        sidebar.grid(row=0, column=0)
+
+        self.sidebar = sidebar
+
+        os.chdir(os.environ["USER" if os.name != "nt" else "userprofile"])
+
+        for folderOrFile in os.listdir():
+            print(f'[{"DIR" if os.path.isdir(os.path.join(os.getcwd(), folderOrFile)) else "FILE"}] ', end=f"{folderOrFile}\n")
+
 
 root = Tk()
 app = App(root)
